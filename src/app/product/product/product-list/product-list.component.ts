527bdf399348f6a0dee934a52acaef0f9c3fec8b
Oscar Fernando Barajas Fuentes <20001572@alumnos.utleon.edu.mx>
2024-09-21 10:15:51 -0600

    Commit sabado 21 sept ejemplo 1

--- src/app/product/product/product-list/product-list.component.ts
+++ src/app/product/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../../productos';
 
 @Component({
   selector: 'app-product-list',
@@ -7,34 +8,43 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+  imageWidth:number=60;
+  imageMargin:number=2;
+  muestraImg:boolean=true;
+  listFilter:string=''
 
-  productos:any[]=[
+  muestImg():void{
+    this.muestraImg=!this.muestraImg;
+  }
+
+
+  productos:IProductos[]=[
     {
       "productoId":1,
       "Modelo":"Sentra",
       "Descripcion":"4 puertas",
-      "Precio":2000,
-      "Year":2023,
+      "Precio":150000,
+      "Year":"marzo 3 2023",
       "Marca":"NISSAN",
       "Color":"Azul",
-      "ImagenUrl":"https://static.wikia.nocookie.net/heroe/images/4/44/Rayo_McQueen_CAeeC.png/revision/latest?cb=20231118102622&path-prefix=es"
+      "ImagenUrl":"https://www.nissanchilpancingo.com.mx/resourcefiles/newvehicleimages/3N1AB8AE9MY332377-resourcefiles-newvehicleimages-nissan-2021-sentra-advance-tm-rojo-burdeos-negro-exterior-v1-d.jpg"
     },
     {
       "productoId":2,
       "Modelo":"Rio",
       "Descripcion":"4 puertas",
       "Precio":150000,
-      "Year":2020,
+      "Year":"julio 1 2020",
       "Marca":"KIA",
       "Color":"Azul",
-      "ImagenUrl":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.wikipedia.org%2Fwiki%2FTroll_%2528automoci%25C3%25B3n%2529&psig=AOvVaw3YsOSnJLb8gGnWL2ZVQmZd&ust=1726971636459000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOj17On80ogDFQAAAAAdAAAAABAJ"
+      "ImagenUrl":"https://dealers.kia.com/content/dam/kwcms/mx/es/images/showroom/2022/forte-sd-2022/kia_ForteSD.png"
     },
     {
       "productoId":3,
       "Modelo":"BOCHO",
       "Descripcion":"2 puertas",
-      "Precio":1500,
-      "Year":2021,
+      "Precio":150000,
+      "Year":"junio 8 2021",
       "Marca":"VOLKSWAGEN",
       "Color":"Azul",
       "ImagenUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDtCAX2nLf1szk9Km0p7BQuH6IPn5Y-JDZAA&s"
---
+++ src/app/product/productos.ts
@@ -0,0 +1,11 @@
+export interface IProductos {
+    productoId:number;
+      Modelo:string;
+      Descripcion:string,
+      Precio:150000,
+      Year:string,
+      Marca:string,
+      Color:string,
+      ImagenUrl:string
+}
+
